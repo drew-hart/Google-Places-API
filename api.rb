809fc563e479966f67924804bbd785fca9d1a7c3
Drew Hart <andrewmhart@gmail.com>
2013-05-26 16:31:33 -0500

    script now returns the valu of key  = name

--- api.rb
+++ api.rb
@@ -6,7 +6,7 @@ latitude =  43.0389
 longitude = -87.9064
 sensor = false
 keyword = "food"
-radius = 5000
+radius = 50000
 key = "AIzaSyBH11S2JGj5NBPghmgdid4Z7PXHM8RTFhI"
 api_call = "https://maps.googleapis.com/maps/api/place/nearbysearch/json\?&location\=#{latitude},#{longitude}&radius\=#{radius}&sensor\=#{sensor}&key\=#{key}&keyword=#{keyword}"
 
@@ -14,4 +14,6 @@ file = File.open('json.txt', 'r').read
 jdoc = JSON.parse(file)
 #jdoc = JSON.parse(open(api_call).read)
 
-puts jdoc.fetch("status")
+jdoc.fetch("results").each do |item|
+  puts item.fetch("name")
+end
