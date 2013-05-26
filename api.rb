16add02da7e19837ed302a78ae6d79cfaabb10d9
Drew Hart <andrewmhart@gmail.com>
2013-05-26 16:25:17 -0500

    inital json fetch working

---
+++ api.rb
@@ -0,0 +1,17 @@
+require 'open-uri'
+require 'json'
+require 'pp'
+
+latitude =  43.0389
+longitude = -87.9064
+sensor = false
+keyword = "food"
+radius = 5000
+key = "AIzaSyBH11S2JGj5NBPghmgdid4Z7PXHM8RTFhI"
+api_call = "https://maps.googleapis.com/maps/api/place/nearbysearch/json\?&location\=#{latitude},#{longitude}&radius\=#{radius}&sensor\=#{sensor}&key\=#{key}&keyword=#{keyword}"
+
+file = File.open('json.txt', 'r').read
+jdoc = JSON.parse(file)
+#jdoc = JSON.parse(open(api_call).read)
+
+puts jdoc.fetch("status")
