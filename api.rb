389438549f11cd6e8f0d6f9450f7435924f3c4c8
Drew Hart <andrewmhart@gmail.com>
2013-05-26 23:12:56 -0500

    added comments / formatting

--- api.rb
+++ api.rb
@@ -10,10 +10,35 @@ radius = 50000
 key = "AIzaSyBH11S2JGj5NBPghmgdid4Z7PXHM8RTFhI"
 api_call = "https://maps.googleapis.com/maps/api/place/nearbysearch/json\?&location\=#{latitude},#{longitude}&radius\=#{radius}&sensor\=#{sensor}&key\=#{key}&keyword=#{keyword}"
 
+
 file = File.open('json.txt', 'r').read
 jdoc = JSON.parse(file)
 #jdoc = JSON.parse(open(api_call).read)
 
 jdoc.fetch("results").each do |item|
+
+# Place name
   puts item.fetch("name")
+  reference = item.fetch("reference")
+
+# Snag place details
+  ref_api_call = "https://maps.googleapis.com/maps/api/place/details/json\?&sensor\=#{sensor}&key\=#{key}&reference=#{reference}"
+  ref_jdoc = JSON.parse(open(ref_api_call).read)
+ 
+# Iterate through Place formatted address and blank out the last entry (i.e. country)
+  ref_jdoc.fetch("result").fetch("formatted_address").split(", ").each() do |value|
+    puts value.gsub(/United States/,"")
+  end
+ 
+# phone number
+ puts ref_jdoc.fetch("result").fetch("formatted_phone_number")
+  puts "-" * 40
+
+
+#  puts item.fetch("vicinity")
+#  item.fetch("types").each do |value|
+#    print value, ', '
+#  end
+#  puts "\n"
+#  puts "=" * 80
 end
